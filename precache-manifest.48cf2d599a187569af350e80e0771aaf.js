6a3c1daab8cb3db3f8fa3f3d98bd69787acae2e6
anatoly <tolik.h@playscape.co.il>
2019-03-30 21:35:15 +0300

    Updates

---
+++ precache-manifest.48cf2d599a187569af350e80e0771aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3950117a3d78c343f4f",
+    "url": "/Galibov/fx-charts/static/js/runtime~main.b27609b0.js"
+  },
+  {
+    "revision": "9e3c1528db680bd5d368",
+    "url": "/Galibov/fx-charts/static/js/main.96c593c4.chunk.js"
+  },
+  {
+    "revision": "a8c22f5190a96b9a4edf",
+    "url": "/Galibov/fx-charts/static/js/2.0874961d.chunk.js"
+  },
+  {
+    "revision": "9e3c1528db680bd5d368",
+    "url": "/Galibov/fx-charts/static/css/main.a56674d0.chunk.css"
+  },
+  {
+    "revision": "a8c22f5190a96b9a4edf",
+    "url": "/Galibov/fx-charts/static/css/2.f1edd557.chunk.css"
+  },
+  {
+    "revision": "9bd125b5a741b33cb2299b72e16a30b4",
+    "url": "/Galibov/fx-charts/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Galibov/fx-charts/precache-manifest.48cf2d599a187569af350e80e0771aaf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Galibov/fx-charts/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
